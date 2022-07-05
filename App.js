20dadd8b6b431e80a0f5b0102f2b2f1c4db2676c
Manuel Barraza <barrazaalexmanuel@gmail.com>
2022-07-04 20:39:12 -0600

    Feat (DVT Stage): Color gradient and some elements added to login screen

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {LinearGradient} from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.contenedorprincipal}>
+    <LinearGradient
+        colors={['#D8A31A','#002244','#53273C','#110000']}
+        style={styles.background}
+      />
+    
+    <Text style={styles.welcome}>Welcome to Scan 'N Go</Text>
+    <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttontext}>Login in</Text>
+    </TouchableOpacity>
+
+    
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  contenedorprincipal: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
-});
+  welcome:{
+    color: '#ffffff',
+    fontSize: 34
+  },
+  button:{
+    backgroundColor: '#ffffff',
+    padding: 10,
+    marginTop: 15,
+    width: 200,
+    textAlign: 'center',
+    alignContent: 'center',
+    alignContent: 'center',
+    borderRadius: 25
+  },
+  buttontext:{
+    fontSize: 20,
+    textAlign: 'center'
+  },
+  background:{
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: 1000,
+  }
+
+
+
+})
+
+
---
+++ src/login.js
@@ -0,0 +1,13 @@
+import { StyleSheet, View, Text, Button, Image, TouchableOpacity } from "react-native-web";
+import React from 'react'
+
+export default function App(){
+    return(
+        <View>
+            <Text>Welcome to Scan 'N Go</Text>
+            <TouchableOpacity>
+                <Text>Login in</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
